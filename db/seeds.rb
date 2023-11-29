4e7773722e5b52b5a9c9f40d03c07ffcf134c948
Abel Lavieri <abel.lavieri@gmail.com>
2023-11-29 05:10:55 -0400

    Add greeting model and db:seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Seeding the greetings to the database ...'
+
+['Greetings earthling!', 'Hello gentlefriend!', 'Well hello hello', 'Aloha', 'Salutations!'].each do |greeting|
+  Greeting.find_or_create_by!(display_text: greeting)
+  puts "Created the greeting: #{greeting}."
+end
+
+puts 'Greetings created.'
