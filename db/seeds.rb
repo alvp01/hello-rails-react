0640d1f82ec551d59a7cffb814ccbdcfaa29eab0
Abel Lavieri <abel.lavieri@gmail.com>
2023-12-02 04:10:41 -0400

    Add linters

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 puts 'Seeding the greetings to the database ...'
 
 ['Greetings earthling!', 'Hello gentlefriend!', 'Well hello hello', 'Aloha', 'Salutations!'].each do |greeting|
