9a7e716269eaf3a853811a1cb00b6bae4057f006
Abel Lavieri <abel.lavieri@gmail.com>
2023-12-04 21:10:25 -0400

    Fix linters file error, and fix review comment

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import GreetingComponent from './Greeting';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../redux/store';
 
-const App = () => {
-  const greeting = useSelector((state) => state.greeting.greeting);
-
-  return <GreetingComponent greeting={greeting} />;
-};
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<GreetingComponent />} />
+      </Routes>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
